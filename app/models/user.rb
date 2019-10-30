286b59bdda30b315a08493ac9a8fa01665ee0c97
Tatehito <tatehito.st@gmail.com>
2019-10-30 21:48:12 +0900

    Add validation to User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates :provider, :uid, :user_name, presence: true
+  validates :provider, uniqueness: { scope: :uid }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,51 +2,49 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  # validation
-  it "is valid with a provider, uid, and user_name" do
-    expect(FactoryBot.build(:user)).to be_valid
+  describe 'validations' do
+    it "is valid with a provider, uid, and user_name" do
+      expect(FactoryBot.build(:user)).to be_valid
+    end
+
+    it "is invalid without a provider" do
+      user = FactoryBot.build(:user, provider: nil)
+      user.valid?
+      expect(user.errors[:provider]).to include("can't be blank")
+    end
+
+    it "is invalid without a uid" do
+      user = FactoryBot.build(:user, uid: nil)
+      user.valid?
+      expect(user.errors[:uid]).to include("can't be blank")
+    end
+
+    it "is invalid without a user_name" do
+      user = FactoryBot.build(:user, user_name: nil)
+      user.valid?
+      expect(user.errors[:user_name]).to include("can't be blank")
+    end
+
+    context 'duplicate errors' do
+      before do
+        FactoryBot.create(:user)
+      end
+
+      it "is invalid with a duplicate provider and uid" do
+        user = FactoryBot.build(:user, user_name: "Taro")
+        user.valid?
+        expect(user.errors[:provider]).to include("has already been taken")
+      end
+
+      it "is valid with a diffent provider and a same uid" do
+        user = FactoryBot.build(:user, provider: "Facebook")
+        expect(user.valid?).to be_truthy
+      end
+
+      it "is valid with a same provider and a diffent uid" do
+        user = FactoryBot.build(:user, uid: "987654321")
+        expect(user.valid?).to be_truthy
+      end
+    end
   end
-
-  # it "is invalid without a provider" do
-  #   user = User.new(provider: nil)
-  #   user.valid?
-  #   expect(user.errors[:provider]).to include("can't be blank")
-  # end
-
-  # it "is invalid without a uid" do
-  #   user = User.new(uid: nil)
-  #   user.valid?
-  #   expect(user.errors[:uid]).to include("can't be blank")
-  # end
-
-  # it "is invalid without a user_name" do
-  #   user = User.new(user_name: nil)
-  #   user.valid?
-  #   expect(user.errors[:user_name]).to include("can't be blank")
-  # end
-
-  # it "is invalid with a duplicate provider and uid" do
-  #   User.create(
-  #     provider: "Twitter",
-  #     uid: "123456789",
-  #     user_name: "tatehito",
-  #   )
-
-  #   user = User.new(
-  #     provider: "Twitter",
-  #     uid: "123456789",
-  #     user_name: "joker",
-  #   )
-
-  #   user.valid?
-  #   expect(user.errors[:provider]).to include("has already been taken")
-  #   expect(user.errors[:uid]).to include("has already been taken")
-  # end
-
-  # it "is valid with a diffent provider and a same uid" do
-  # end
-
-  # it "is valid with a same provider and a diffent uid" do
-  # end
-
 end
