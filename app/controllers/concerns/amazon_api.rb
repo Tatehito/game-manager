70c7966160ee032bc0e1bdddb0954893e08d711b
Tatehito <tatehito.st@gmail.com>
2019-12-20 00:03:02 +0900

    Update search results page layout.

--- app/controllers/concerns/amazon_api.rb
+++ app/controllers/concerns/amazon_api.rb
@@ -2,11 +2,37 @@ module AmazonApi
   require 'amazon/ecs'
 
   def amazon_search(word)
-    Amazon::Ecs.item_search(
+    res = Amazon::Ecs.item_search(
         word,
         country: 'jp',
         response_group: 'ItemAttributes, Images',
         search_index: 'VideoGames'
     )
+
+    res.items.map do |item|
+      average = evaluation_average(item.get('ASIN'))
+      game_info(
+          CGI.unescapeHTML(item.get('ItemAttributes/Title')),
+          item.get('LargeImage/URL'),
+          CGI.unescapeHTML(item.get('ItemAttributes/Manufacturer')),
+          Game.where(asin: item.get('ASIN')).count,
+          average.nil? ? '-' : average)
+    end
+  end
+
+  private
+
+  def game_info(title, image, manufacturer, count, average)
+    {
+        title: title,
+        image: image,
+        manufacturer: manufacturer,
+        count: count,
+        average: average
+    }
+  end
+
+  def evaluation_average(asin)
+    Game.where(asin: asin).average(:evaluation)
   end
 end
