f549a91ac358e47a15a1143625742ba963964d91
Tatehito <tatehito.st@gmail.com>
2019-12-19 21:57:57 +0900

    Display search results on the pre-login page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,23 +1,54 @@
 class PagesController < ApplicationController
+  include AmazonApi
+
   def index
     redirect_to games_path if logged_in?
+    @games = params[:search_word].nil? ? popular_games : search_results
+  end
 
+  private
+
+  def popular_games
     to = Time.now
     from = to - 1.month
-    asin = Game.where(created_at: from...to).group(:asin).order("count_all DESC").limit(5).count
+    asin = Game.where(created_at: from...to).group(:asin).order("count_all DESC").limit(10).count
 
-    @popular_games = asin.keys.map do |a|
+    asin.keys.map do |a|
       game = Game.find_by(asin: a)
-      average = Game.where(asin: a).average(:evaluation)
-      average = average.nil? ? '-' : average
-      {
-        # TODO 編集後のタイトルが表示されてしまう
-        title: game.title,
-        image: game.image,
-        manufacturer: game.manufacturer,
-        count: Game.where(asin: a).count,
-        average: average
-      }
+      average = evaluation_average(a)
+      game_info(
+          game.title,
+          game.image,
+          game.manufacturer,
+          Game.where(asin: a).count,
+          average.nil? ? '-' : average)
     end
+  end
+
+  def search_results
+    res = amazon_search(params[:search_word])
+    res.items.map do |item|
+      average = evaluation_average(item.get('ASIN'))
+      game_info(
+          CGI.unescapeHTML(item.get('ItemAttributes/Title')),
+          item.get('LargeImage/URL'),
+          CGI.unescapeHTML(item.get('ItemAttributes/Manufacturer')),
+          Game.where(asin: item.get('ASIN')).count,
+          average.nil? ? '-' : average)
+    end
+  end
+
+  def game_info(title, image, manufacturer, count, average)
+    {
+        title: title,
+        image: image,
+        manufacturer: manufacturer,
+        count: count,
+        average: average
+    }
+  end
+
+  def evaluation_average(asin)
+    Game.where(asin: asin).average(:evaluation)
   end
 end
