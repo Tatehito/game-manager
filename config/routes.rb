d1649f55aa26e54874205db1e7a71837a7e07b82
Tatehito <tatehito.st@gmail.com>
2019-12-22 22:59:39 +0900

    Fix login cancellation bug.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#index'
   get '/auth/:provider/callback', to: 'sessions#create'
+  get '/auth/failure', to: 'sessions#failure'
   get '/signout', to: 'sessions#destroy'
   get '/pre_search', to: 'pages#search'
   get '/search', to: 'search#index'
